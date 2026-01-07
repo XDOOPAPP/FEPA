9c3805f9a0a1eda78a770a632f3228af3ae1fcb2
Loi1966 <loitt0503@ut.edu.vn>
2026-01-07 18:51:43 +0700

    App Main & giao diện

--- Webadmin/src/main.tsx
+++ Webadmin/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root')
+if (!rootElement) {
+  throw new Error('Root element not found')
+}
+
+ReactDOM.createRoot(rootElement).render(<App />)
